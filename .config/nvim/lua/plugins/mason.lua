8ba5c6b611303cbf8e7ca04af2241ea74c489990
Bahaa Zahika <bahaaz@mobileye.com>
2024-04-09 11:22:36 +0300

    nvim :: add mason pre installs

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -6,6 +6,15 @@ return {
       "flake8",
       "bash-language-server",
       "autopep8",
+      "stylua",
+      "dockerfile-language-server",
+      "docker-compose-language-service",
+      "lua-language-server",
+      "yaml-language-server",
+      "typescript-language-server",
+      "eslint-lsp",
+      "json-lsp",
+      "terraform-ls"
     },
   },
 }
