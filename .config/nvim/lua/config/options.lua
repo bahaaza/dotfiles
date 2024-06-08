b66a3b7158f15a1b2b1d3b33ebb815e99079bca3
Bahaa Zahika <bahaaz@mobileye.com>
2024-06-08 14:37:12 +0300

    neovim :: configuratios

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,6 +3,21 @@
 -- Add any additional options here
 local opt = vim.opt
 
+vim.g.vindent_motion_OO_prev   = '[='
+vim.g.vindent_motion_OO_next   = ']='
+vim.g.vindent_motion_more_prev = '[+'
+vim.g.vindent_motion_more_next = ']+'
+vim.g.vindent_motion_less_prev = '[-'
+vim.g.vindent_motion_less_next = ']-'
+vim.g.vindent_motion_diff_prev = '[;'
+vim.g.vindent_motion_diff_next = '];'
+vim.g.vindent_motion_XX_ss     = '[p'
+vim.g.vindent_motion_XX_se     = ']p'
+vim.g.vindent_object_XX_ii     = 'ii'
+vim.g.vindent_object_XX_ai     = 'ai'
+vim.g.vindent_object_XX_aI     = 'aI'
+vim.g.vindent_jumps            = 1
+
 opt.clipboard = ""
 opt.relativenumber = true
 vim.opt.colorcolumn = "120"
